4c27abc61905bab0edadccc380c0b09d414ad155
Sarat.Bharadwaj@iiitb.ac.in <saratcbharadwaj@gmail.com>
2025-02-18 07:02:21 +0530

    Added ch1c2.cpp

---
+++ chapter1/ch1c2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+	int f;
+	cin >> f;
+	int c = (f - 32) * (5 / 9);
+
+	cout << c;
+	for (int j = 10; j <= 100; j++) {
+
+		cout << endl <<  j << endl;
+	}
+
+	return 0;
+}
